828482633434ef6a2d84d3681bc4ce2d8150c306
Thomas van Otterloo <vanotterloo.thomas@gmail.com>
2024-12-09 22:02:43 +0100

    Use of Vertex Arrays Objects

--- Project1/src/Application.cpp
+++ Project1/src/Application.cpp
@@ -122,6 +122,9 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -153,6 +156,9 @@ int main(void)
 		2, 3, 0
 	};
 
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
@@ -160,7 +166,7 @@ int main(void)
 	glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW); // refers to the size of the positions array
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); // links
 
 	/*unsigned int shader = CreateShader(vertexShader, fragmentShader);
 	glUseProgram(shader);*/
@@ -184,6 +190,14 @@ int main(void)
 
 
 
+	//Unbounds everything
+	GLCall(glBindVertexArray(0));
+	GLCall(glUseProgram(0));\
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
+
+
 
 	float r = 0.0f;
 	float increment = 0.05f;
@@ -193,9 +207,16 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+		GLCall(glUseProgram(shader));
 		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
-        GLCall(glDrawElements(GL_TRIANGLES,  6, GL_UNSIGNED_INT, nullptr));
+
+		
+		GLCall(glBindVertexArray(vao));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
+
 	
+        GLCall(glDrawElements(GL_TRIANGLES,  6, GL_UNSIGNED_INT, nullptr));
 		if (r > 1.0f)
 			increment = -0.05f;
 		else if (r < 0.0f)
